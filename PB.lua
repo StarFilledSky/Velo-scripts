4ebbd1d82fd78c00c35dc1529d9a950cf8bc19dc
sky!! <jbofl15@gmail.com>
2026-01-12 00:24:36 -0500

    added PB

---
+++ PB.lua
@@ -0,0 +1,70 @@
+--[[
+
+gets the players pb for the current map and sets it to a ghost
+
+--]]
+
+
+if not get("Velo.isIngame") or get("Velo.isOnline") then
+    return;
+end
+
+a = get("Player#1.name")
+
+if type(a) ~= "string" then
+    echo("making a ghost..ok?")
+    prepareGhost(0)
+end
+
+steamId = get("Velo.mySteamId")
+mapId = get("Velo.mapId")
+-- requestId = nil
+
+-- the map ids for rws and officals are all below 200 or something
+--this just selects
+if mapId < 300 then 
+    requestId = requestPlayerPBRuns(steamId, 0)
+else
+    requestId = requestPlayerPBRuns(steamId, 1)
+end
+
+onReceiveRuns = function(requestId_, runs)
+    if requestId ~= requestId_ then return end
+   
+    echo("got the runs..ok?")
+    
+    mapPB = nil
+    runtime = 999999
+    echo(tostring(mapId))
+    for k, v in pairs(runs) do
+
+        if mapId == v.mapId and v.categoryId == NEW_LAP then
+            
+            if runtime  > v.runTime then
+                
+                mapPB = v
+                runtime = v.runTime
+            end
+        end
+    end
+
+    if mapPB == nil then
+        echo("no runs found for this map..ok?")
+        exit()
+    end
+
+    download(mapPB.id, "run_"..tostring(mapPB.id))
+
+end
+
+
+onDownloadFinished = function(requestId_, id, name)
+    -- if requestId ~= requestId_ then return end -- not working don't care enough to look into it
+
+    echo("setting ghost..ok?")
+
+    setGhost(name, 0)
+    deleteRec(name)
+
+    exit()
+end
