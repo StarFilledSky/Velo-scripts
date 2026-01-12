40ce88da757f45cdd81816da509f9811428819e6
sky!! <239486298+StarFilledSky@users.noreply.github.com>
2026-01-11 19:10:10 -0500

    i don't remember what i changed here
    uhh uhm

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a my collection of QOL velo scripts
+
--- dstart.lua
+++ dstart.lua
@@ -1,10 +1,16 @@
 --[[ delayed start
 A velo script for Speedrunners that resets a solo run lap with a delay
+
+todo
+
+- add restart on no movement
+- restart to a savestate
+
 --]]
 
 
 --[[PROGRAM VARIABLES]]
-timer = 0
+wait_time = 0
 countdown_start = 0
 countdown_end = 0
 display_time = ""
@@ -20,20 +26,29 @@ height = get("Velo.screenHeight")
 
 
 --[[USER CHANGABLE VARIABLES]]
+-- you can find the keys https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+hotkey = 0x42 -- b by default
+
+-- wait times for different resets in seconds
 hotkey_countdown_timer = 0.5 -- how long before the lap resets when the hotkey is pressed in seconds
-lap_countdown_timer = 1.5 -- how long before the lap resets in seconds on completion of a lap
+lap_countdown_timer = 1.5 -- how long before the lap resets on completion of a lap
+stun_countdown_timer = 0.3 -- how long before the lap resets when the player is stunned
+
 display_num = 3 -- the number the display counts down from is real_time_display is false 3..2..1..
 real_time_display = false -- whether it counts down from the display number or the realtime 
-restart_on_lap = true -- whether the countdown starts on every new lap
--- display text settings
 
-pos = Vector2:new(0, 0)
+restart_on_lap = true -- resets when a lap is completed
+restart_on_stun = false -- resets the lap when player is stunned
+
+
+-- display text settings
+pos = Vector2:new(0, 0) -- x and y pixel position on the screen starts from top left
 size = Vector2:new(width, height)
 alignment = Vector2:new(0.5, 0.2) -- % relative position from the pos variable
-scale = 2
+scale = 1
 rotation = 0
 font = "UI\\Font\\Souses.ttf"
-font_size = 40
+font_size = 65
 drop_shadow = true -- drop shadow
 c = Color:new(252, 147, 170)
 
@@ -41,12 +56,11 @@ c = Color:new(252, 147, 170)
 
 -- fires when inputs are polled
 onSetInputs = function()
-    -- keys https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-    key = 0x42
-    if isReleased(key) and state == STANDBY then -- on press initialize the reset
+
+    if isReleased(hotkey) and state == STANDBY then -- on press initialize the reset
         state = INITIALIZATION
-        timer = hotkey_countdown_timer
-    elseif isReleased(key) and state == COUNTDOWN then -- stops the timer if pressed again
+        wait_time = hotkey_countdown_timer
+    elseif isReleased(hotkey) and state == COUNTDOWN then -- stops the countdown if pressed again
         state = STANDBY
     end
 end
@@ -54,16 +68,29 @@ end
 onLapFinish = function()
     if restart_on_lap then
         state = INITIALIZATION
-        timer = lap_countdown_timer
+        wait_time = lap_countdown_timer
+    end
+end
+
+function stunCheck()
+    if restart_on_stun then
+        status = get("Player#0.isStunned")
+        if status then
+            state = INITIALIZATION
+            wait_time = stun_countdown_timer
+        end
     end
 end
 
 onPostUpdate = function()
+    stunCheck()
+
+
     if state == INITIALIZATION then
         state = COUNTDOWN
         countdown_start = get("Velo.realTime")
         -- converted to int to avoid issues with double * int
-        countdown_end = countdown_start + math.tointeger(timer * TICKS_PER_SECOND)
+        countdown_end = countdown_start + math.tointeger(wait_time * TICKS_PER_SECOND)
 
     elseif state == COUNTDOWN then
         countdown_current = get("Velo.realTime") -- gets system time in game ticks
@@ -76,10 +103,10 @@ onPostUpdate = function()
             elapsed_time = ((countdown_current - countdown_start) /  TICKS_PER_SECOND)
             
             if real_time_display then
-                display_time = "Resetting in " .. string.format("%.2f", timer - elapsed_time) 
+                display_time = "Resetting in " .. string.format("%.2f", wait_time - elapsed_time) 
             else
                 -- mapping the countdown value to a 3,2,1 or whatever the display_num starts with
-                mapped_elapse = elapsed_time * (display_num / timer)
+                mapped_elapse = elapsed_time * (display_num / wait_time)
                 display_time = "Resetting in " .. tostring(math.floor(display_num + 1 - mapped_elapse))
             end
         end
