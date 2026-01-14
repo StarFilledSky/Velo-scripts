e7a58d8e1fa3be61a2518195e1005c65d7bfea38
sky!! <jbofl15@gmail.com>
2026-01-13 23:52:20 -0500

    made global variables local and added an oversurf example

--- surfexample.lua
+++ surfexample.lua
@@ -3,35 +3,39 @@ if not success then
     echo("Module failed to load: " .. Surf)
 end
 
-s = Surf:new()
+local s = Surf:new()
 
 
  -- i think playsound starts in the content folder but it might also check other places idk
  -- can confirm wav/xnb files work, haven't/don't remember testing other file formats
-audio = "Audio/Sfx/Weapons/ch_freeze.xnb"
-volume = 0.5
-pitch = 0
+local surf_start_audio = "Audio/Sfx/Weapons/ch_freeze.xnb"
+local oversurf_audio = "Audio/Sfx/Weapons/item_rocket_launch_1.xnb"
+local volume = 0.5
+local pitch = 0
 
 
-surf_start = get(s.player .. ".actor.position")
-surf_end = get(s.player .. ".actor.position")
+local surf_pos_start = get(s.player .. ".actor.position")
+local surf_pos_end = get(s.player .. ".actor.position")
 
-horizontal_offset = Vector2:new(25, 0)
-vertical_offset = Vector2:new(0, 45)
+local horizontal_offset = Vector2:new(25, 0)
+local vertical_offset = Vector2:new(0, 45)
 
 onPostUpdate = function()
     s:update()
+    if s.paused then
+        return
+    end
     
     if s.surf_started then
-        playSound(audio, volume, pitch, 0) -- remove this if you don't want sound
+        playSound(surf_start_audio, volume, pitch, 0) -- remove this if you don't want sound
         offset = vertical_offset
 
         if get(s.player .. ".moveDirection") == 1 then
             offset = offset + horizontal_offset
         end
         
-        surf_start = get(s.player .. ".actor.position") + offset
-        surf_end = get(s.player .. ".actor.position") + offset
+        surf_pos_start = get(s.player .. ".actor.position") + offset
+        surf_pos_end = get(s.player .. ".actor.position") + offset
 
     end
 
@@ -43,18 +47,22 @@ onPostUpdate = function()
             offset = offset + horizontal_offset
         end
         
-        surf_end = get(s.player .. ".actor.position") + offset
+        surf_pos_end = get(s.player .. ".actor.position") + offset
         
     end
 
     if s.surf_ended then
+        -- todo put something here maybe
+    end
 
+    if s.oversurfed then
+        playSound(oversurf_audio, volume, pitch, 0) -- remove this if you don't want sound
     end
 
 end
 
 onPostDraw = function()
 
-    drawLine(worldToScreen(surf_start), worldToScreen(surf_end), 3, Color:new(255, 0, 0))
+    drawLine(worldToScreen(surf_pos_start), worldToScreen(surf_pos_end), 3, Color:new(255, 0, 0))
 
 end
