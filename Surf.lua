2622d1322ba0bfabee31fcdd761a14b9a27e31b7
sky!! <jbofl15@gmail.com>
2026-01-14 22:01:38 -0500

    just updating this ofr the sake of it

--- Surf.lua
+++ Surf.lua
@@ -34,6 +34,9 @@ function Surf:new(player_number)
     self._was_surfing = false
     self._was_grounded = false
 
+    self.debug_mode = true
+
+
     return obj
 end
 
@@ -57,21 +60,20 @@ function Surf:update()
     -- variable resets
     self.surf_ended = false
     self.oversurfed = false
-    
+    self.speed_gain = -1
 
-
-    _current_position = get(self.player .. ".actor.position")
     _colliding = get(self.player .. ".isCollidingWithSolid")
-    _jumping = get(self.player .. ".jumpHeld")
     _in_air = get(self.player .. ".isInAir")
     _in_boost_tunnel = get(self.player .. ".isInsideSuperBoost")
     _collision_type = get(self.player .. ".groundCollidableType")
     _surfable_tile = false
 
+    -- get("Player.isCollidingWithSolid") -- possible means of detecting slope surfs
+    -- from what i can tell it seems to trigger when a players velocity would put them into a block
+    -- triggers on stairs, gates, roof slopes, walls 
     
     
-    
-    _tile_types = {4, 5, 120}
+    _tile_types = {4, 5, 120} -- tiles for left slope, right slope, and gates
     for i, t in ipairs(_tile_types) do
         if _collision_type == t then
             _surfable_tile = true
@@ -93,6 +95,26 @@ function Surf:update()
         self.surfing = true
 
     elseif self._was_surfing and not _is_surfing then -- end of surf
+        
+        if self.debug_mode then
+            debug_info = "surf ended\n"
+            .. "surfable tile: " 
+            .. tostring(_surfable_tile) 
+            .. " | " 
+            .. "in air: " 
+            .. tostring(_in_air) 
+            .. " | " 
+            .. "colliding: " 
+            .. tostring(_colliding) 
+            .. " | " 
+            .. "was not grounded: " 
+            .. tostring(not self._was_grounded) 
+            .. " | " 
+            .. "not in boost tunnel: " 
+            .. tostring(not _in_boost_tunnel)
+            echo(debug_info)
+        end
+
         self._was_surfing = false
         self._was_grounded = true
 
@@ -100,6 +122,7 @@ function Surf:update()
         self.surfing = false
         self.surf_ended = true
         self.oversurfed = _in_air
+
     end
 
     
@@ -111,8 +134,6 @@ function Surf:update()
     else
         self._was_grounded = false
     end
-
-    self._previous_position = _current_position
 
 end
 
