6c8003b0ae4f10a389c16d648ac0eafbe3c65f89
sky!! <jbofl15@gmail.com>
2026-01-15 21:59:17 -0500

    added commented out version with a polling rate

--- fps.lua
+++ fps.lua
@@ -18,3 +18,31 @@ onPostUpdate = function()
         setSt(frame_limit, capped)
     end
 end
+
+
+-- added a polling rate but i don't think it really matters here unless i'm sharing the polling variable with other programs
+-- because this one only calls Velo.isInGame
+-- i don't have a means for benchmarking atm so doesn't matter anyway
+
+-- local capped = 60
+-- local max = 300
+-- local polling_rate = 0.5
+-- local timer = polling_rate
+
+-- local frame_limit = "Performance.framerate.framelimit"
+
+-- onPostUpdate = function()
+--     timer = timer - get("Velo.realDeltaSec")
+--     if timer <= 0 then
+--         timer = polling_rate + timer
+
+--         fps = getSt(frame_limit)
+--         in_game = get("Velo.isIngame")
+
+--         if in_game and fps ~= max then
+--             setSt(frame_limit, max)
+--         elseif not in_game and fps ~= capped then
+--             setSt(frame_limit, capped)
+--         end
+--     end
+-- end
