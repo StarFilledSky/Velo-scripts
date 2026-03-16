fe1f321d5b2be73dc447f4436b72613659f7167d
sky <jbofl15@gmail.com>
2026-03-16 00:42:14 -0400

    adding clear ghosts

---
+++ cg.lua
@@ -0,0 +1,12 @@
+-- clear ghosts whenever the player gets reset
+-- so like anytime you reset round it clears ghosts(fires like twice but the int returns 0 for some reason)
+-- but point being the ghost goes away ok thx
+-- kinda untested but i will toss it on here bc someone will probably find use for it
+
+
+-- todo write a more fitting description or smth 
+
+
+onPlayerReset = function(idk)
+clearGhosts()
+end
