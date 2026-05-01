80f3911bf74abc02200f1cf92077a972cb73a8e6
sky <jbofl15@gmail.com>
2026-05-01 19:57:09 -0400

    aa

--- test2.lua
+++
@@ -1,8 +0,0 @@
-for i = 1, 10, 1 do
-    local test = "this should be defined before the other test"
-    echo("test string " .. tostring(test))
-end
-
-local test = "this is a test string"
-
-echo("test string " .. tostring(test))
